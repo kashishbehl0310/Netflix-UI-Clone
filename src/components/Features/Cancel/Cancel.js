c90578879a827a0b36d4d31b84fba61022e14abb
kashish <kashishbehl36@gmail.com>
2019-04-08 15:23:12 +0530

    Cancel component

--- src/components/Features/Cancel/Cancel.js
+++ src/components/Features/Cancel/Cancel.js
@@ -1,9 +1,18 @@
 import React from "react";
+import cancelImage from './icons/cancel.png';
 
 const Cancel = () => {
     return(
         <section className="cancleWrapper">
-            
+            <div className="cancelcolumns">
+                <div className="cancelcolumn">
+                    <h2>If you decide Netflix isn't for you - No problem. No Commitment. Cancel online anytime.</h2>
+                    <button>Join free for a month</button>
+                </div>
+                <div className="cancelcolumn">
+                    <img src={cancelImage} alt="Cancel Subscription anytime" />
+                </div>
+            </div>
         </section>
     )
 }
