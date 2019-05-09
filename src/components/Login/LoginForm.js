779da9fbfcbe26b8f11209dd5eb25784e6407dcc
kashishbehl0310 <kashishbehl36@gmail.com>
2019-05-10 00:04:30 +0530

    Authentication UI

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,16 +1,52 @@
 import React, {Component} from 'react';
+import axios from "axios";
 
 export class LoginForm extends Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            email: '',
+            password: ''
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    onChange(e){
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+    onSubmit(e){
+        e.preventDefault();
+        const { email, password } = this.state;
+        axios.post('http://localhost:3000/api/users/login', {email, password})
+            .then((result) => {
+                localStorage.setItem('jwtToken', result.data.token);
+                console.log("logged in successfully")
+            })
+            .catch((error) => {
+                 console.log(error)
+            })
+    }
     render(){
         return(
             <div className="login-body">
                 <div>
                     <div className="login-content">
                         <h1>Sign In</h1>
-                        <form>
-                            <input type="text" name="email" placeholder="Email" />
-                            <input type="password" name="password" placeholder="password" />
-                            <button>Sign In</button>
+                        <form onSubmit={this.onSubmit} >
+                            <fieldset>
+                                <input type="text" name="email" value={this.state.email} onChange={this.onChange} />
+                                <label for="email" >Email</label>
+                            </fieldset>
+                            <fieldset>
+                                <input type="password" name="password" value={this.state.password} onChange={this.onChange} />
+                                <label for="password" >Password</label>
+                            </fieldset>
+                            <button className="signup-button" >Sign In</button>
+                            <div className="signin-now">
+                                New to Netflix? <a href="/signin">Sign up now</a>.
+                            </div>
                         </form>
                     </div>
                 </div>
