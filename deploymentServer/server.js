729da01aed9cd8bed2c68803c483273deb7b950c
Kashish <kashish.behl@holidayiq.com>
2019-08-07 12:03:27 +0530

    Deployment to heroku

--- deploymentServer/server.js
+++ deploymentServer/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000 ;
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'))
+    res.sendFile(path.resolve(publicPath, 'index.html'))
 })
 
 app.listen(port, () => {
