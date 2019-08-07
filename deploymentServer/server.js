0fc13d17f7a9c2457a83c5bea7b9fbfa73318b72
Kashish <kashish.behl@holidayiq.com>
2019-08-07 12:24:34 +0530

    Deployed App to heroku

--- deploymentServer/server.js
+++ deploymentServer/server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000 ;
 
 app.use(express.static(publicPath))
 
-app.get('*', (req, res) => {
-    res.sendFile('index.html', {publicPath})
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', {root: publicPath})
 })
 
 app.listen(port, () => {
